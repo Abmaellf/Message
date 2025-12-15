96efd755b18f57c666d2783527d008f5ad2231c1
Abmaellf <abmael.ninha@gmail.com>
2025-12-14 22:46:50 -0400

    first commit

---
+++ src/main/java/message_full/MessageApplication.java
@@ -0,0 +1,13 @@
+package message_full;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/message_full/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package message_full;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
