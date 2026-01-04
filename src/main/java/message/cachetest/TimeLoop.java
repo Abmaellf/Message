537ba504e654c441777c9be31cd671f3b9bcbf7f
Abmaellf <abmael.ninha@gmail.com>
2026-01-04 00:57:57 -0400

    Testando Spring Boot com Redis como cache

---
+++ src/main/java/message/cachetest/TimeLoop.java
@@ -0,0 +1,26 @@
+package message.cachetest;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Component
+public class TimeLoop {
+    @Autowired StringGenerator stringGenerator;
+
+    @PostConstruct
+    void init() {
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void  run() {
+                String simple = stringGenerator.buildSimpleString();
+                String cached = stringGenerator.buildCachedString();
+
+                System.out.println("Simple: "+simple + " cached: "+cached);
+            }
+        }, 2000L, 2000L);
+    }
+}
